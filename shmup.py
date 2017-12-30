3e7430a3f4e9cb7f34ff65859ad16b866e64221a
Vitali <hoffmann.vitali@gmail.com>
2017-12-30 18:29:42 +0100

    Part 1: Player Sprite and Controls
    Adding the Player class, draw it on the screen, and make possible to move it to left and right.

--- shmup.py
+++ shmup.py
@@ -1,10 +1,10 @@
-# Pygame template - skeleton for a new pygame project
+# Shmup Game
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
 
 # define colors
 WHITE = (255, 255, 255)
@@ -17,10 +17,40 @@ BLUE = (0, 0, 255)
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Shmup!")
 clock = pygame.time.Clock()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        # get_pressed() returnes a list of key events
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 5
+        self.rect.x += self.speedx
+        # create boundaries for the player
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+            
+# A container class to hold and manage multiple Sprite objects.
 all_sprites = pygame.sprite.Group()
+# creating the player object
+player = Player()
+# adding the player object to the sprites group
+all_sprites.add(player)
 # Game loop
 running = True
 while running:

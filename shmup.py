0a482c8552a1a84ca72037229f9cf6a130265e50
Vitali <hoffmann.vitali@gmail.com>
2018-01-15 22:17:27 +0100

    Part 4: Adding Graphics
    In this part the Background was added, the Player img instead of a rect, the mob img and the bullet img.

--- shmup.py
+++ shmup.py
@@ -1,6 +1,10 @@
 # Shmup Game
 import pygame
 import random
+from os import path
+
+# connects to correct path on different computers to retrieve content from img folder
+img_dir = path.join(path.dirname(__file__), 'img')
 
 WIDTH = 480
 HEIGHT = 600
@@ -24,8 +28,8 @@ clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50,40))
-        self.image.fill(GREEN)
+        self.image = pygame.transform.scale(player_img, (50,38))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
@@ -53,8 +57,8 @@ class Player(pygame.sprite.Sprite):
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30,40))
-        self.image.fill(RED)
+        self.image = meteor_img
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
@@ -72,8 +76,8 @@ class Mob(pygame.sprite.Sprite):
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10,20))
-        self.image.fill(YELLOW)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
@@ -84,6 +88,14 @@ class Bullet(pygame.sprite.Sprite):
         #kill if it moves off the top of the screen
         if self.rect.bottom < 0:
             self.kill()
+
+# Load all game graphics
+background = pygame.image.load(path.join(img_dir, "starfield.png")).convert()
+background_rect = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.png")).convert()
+meteor_img = pygame.image.load(path.join(img_dir, "meteorBrown_med1.png")).convert()
+bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
+
 # A container class to hold and manage multiple Sprite objects.
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
@@ -113,7 +125,7 @@ while running:
 
     # Update
     all_sprites.update()
-    
+
     #check to see if a bullet hits a mob
     hits = pygame.sprite.groupcollide(mobs,bullets, True, True)
     for hit in hits:
@@ -128,6 +140,7 @@ while running:
 
     # Draw / render
     screen.fill(BLACK)
+    screen.blit(background, background_rect)
     all_sprites.draw(screen)
     # *after* drawing everything, flip the display
     pygame.display.flip()
